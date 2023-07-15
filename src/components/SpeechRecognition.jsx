05998d5b112cfc9feda67150b5a8ad7375c55d16
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-15 15:21:32 +0200

    Added functionality for clearing and starting and stopping the app

--- src/components/SpeechRecognition.jsx
+++ src/components/SpeechRecognition.jsx
@@ -6,6 +6,7 @@ import SoundWave from "../assets/bars.svg";
 export const SpeechRecognition = () => {
   const [language, setLanguage] = useState("en");
   const [listening, setListening] = useState(false);
+  const [result, setResult] = useState("");
 
   return (
     <div className='w-5/6 h-fit p-4 mt-4 bg-neutral-200 flex flex-col items-center rounded-lg gap-3'>
@@ -29,23 +30,40 @@ export const SpeechRecognition = () => {
       <hr className=' bg-neutral-300  w-full' style={{ height: "2px" }} />
 
       {listening && (
-        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2'>
+        <button
+          onClick={() => {
+            setListening(false);
+          }}
+          className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2'
+        >
           <img src={SoundWave} className='w-6' />
           Listening...
         </button>
       )}
 
       {!listening && (
-        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2'>
+        <button
+          onClick={() => {
+            setListening(true);
+          }}
+          className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2'
+        >
           <BiMicrophone />
           Start Listening
         </button>
       )}
 
       <h1 className='self-start text-xl'>Result:</h1>
-      <div className=' w-full h-36 overflow-y-auto p-4 bg-neutral-100 rounded-lg border-neutral-300 border'></div>
+      <div className=' w-full h-36 overflow-y-auto p-4 bg-neutral-100 rounded-lg border-neutral-300 border'>
+        {result}
+      </div>
       <div className='flex gap-2 w-full'>
-        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm'>
+        <button
+          onClick={() => {
+            setResult("");
+          }}
+          className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm'
+        >
           <BiSolidTrashAlt />
           Clear
         </button>
