4bb4595fdd4e96e40cdec291ba99fe68c8c1fb53
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-15 15:17:59 +0200

    Created the design of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "./components/Navbar";
+import { SpeechRecognition } from "./components/SpeechRecognition";
 
 export const App = () => {
   return (
-    <div className='font-poppins w-full h-screen flex flex-col bg-neutral-100'>
+    <div className='font-poppins w-full h-screen flex flex-col items-center bg-neutral-100'>
       <Navbar />
+      <SpeechRecognition />
     </div>
   );
 };
---
+++ src/components/SpeechRecognition.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import languages from "../assets/Languages";
+import { BiMicrophone, BiSolidTrashAlt, BiDownload } from "react-icons/bi";
+import SoundWave from "../assets/bars.svg";
+
+export const SpeechRecognition = () => {
+  const [language, setLanguage] = useState("en");
+  const [listening, setListening] = useState(false);
+
+  return (
+    <div className='w-5/6 h-fit p-4 mt-4 bg-neutral-200 flex flex-col items-center rounded-lg gap-3'>
+      <h1 className='text-lg font-semibold'>Speech Recognition</h1>
+      <select
+        className='w-full p-1 rounded-md'
+        value={language}
+        onChange={(e) => {
+          setLanguage(e.target.value);
+          console.log(language);
+        }}
+      >
+        {languages.map((lang) => {
+          return (
+            <option key={lang.code} value={lang.code}>
+              {lang.name}
+            </option>
+          );
+        })}
+      </select>
+      <hr className=' bg-neutral-300  w-full' style={{ height: "2px" }} />
+
+      {listening && (
+        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2'>
+          <img src={SoundWave} className='w-6' />
+          Listening...
+        </button>
+      )}
+
+      {!listening && (
+        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2'>
+          <BiMicrophone />
+          Start Listening
+        </button>
+      )}
+
+      <h1 className='self-start text-xl'>Result:</h1>
+      <div className=' w-full h-36 overflow-y-auto p-4 bg-neutral-100 rounded-lg border-neutral-300 border'></div>
+      <div className='flex gap-2 w-full'>
+        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm'>
+          <BiSolidTrashAlt />
+          Clear
+        </button>
+        <button className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm'>
+          <BiDownload />
+          Download
+        </button>
+      </div>
+    </div>
+  );
+};
