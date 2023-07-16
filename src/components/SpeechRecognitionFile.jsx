868695ddceb4ef8def2fe08c1e00789cad975f01
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-16 14:54:41 +0200

    Created file to text

--- src/components/SpeechRecognitionFile.jsx
+++ src/components/SpeechRecognitionFile.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineUpload, AiFillFile, AiOutlineClose } from "react-icons/ai";
+import { BiDownload } from "react-icons/bi";
 
 export const SpeechRecognitionFile = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -7,20 +8,43 @@ export const SpeechRecognitionFile = () => {
   const [error, setError] = useState(null);
 
   const handleFileChange = (event) => {
+    setTranscript("");
     setError(null);
     const file = event.target.files[0];
 
-    if (file.size > 25000000) {
+    if (file && file.size > 25000000) {
       return setError("File must be smaller than 25MB");
     }
     setSelectedFile(file);
   };
 
-  const handleUpload = () => {};
+  const handleUpload = async () => {
+    setTranscript("");
+    setError(null);
+
+    if (!selectedFile) {
+      return setError("Please select a file");
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+    formData.append("model", "whisper-1");
+
+    fetch("https://api.openai.com/v1/audio/transcriptions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => setTranscript(data.text))
+      .catch((error) => console.log(error));
+  };
 
   return (
-    <div className='w-5/6 h-fit p-4 mt-16 bg-neutral-200 flex flex-col items-center rounded-lg gap-3 border-2 border-neutral-300'>
-      <h1 className='text-xl sm:text-2xl font-semibold'>Speech Recognition</h1>
+    <div className='w-5/6 h-fit p-4 mt-10 bg-neutral-200 flex flex-col items-center rounded-lg gap-3 border-2 border-neutral-300'>
+      <h1 className='text-xl sm:text-xl font-semibold'>Speech Recognition</h1>
       <div
         onClick={() => document.querySelector("#file").click()}
         className='w-full h-fit p-8 flex flex-col gap-3 justify-center items-center bg-neutral-300 rounded-md border-dashed border-2 border-sky-900 hover:cursor-pointer'
@@ -52,17 +76,50 @@ export const SpeechRecognitionFile = () => {
         <AiOutlineClose
           className='text-xl hover:cursor-pointer'
           onClick={() => {
+            setError(null);
             setSelectedFile(null);
+            setTranscript("");
+            document.getElementById("file").value = "";
           }}
         />
       </div>
-      <button
-        onClick={handleUpload}
-        className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm sm:text-lg hover:bg-sky-700 transition-all duration-300 ease-in-out'
-      >
-        Upload
-      </button>
-      {transcript && <p>{transcript}</p>}
+      {transcript && (
+        <div className='w-full flex flex-col gap-2'>
+          <h1 className='self-start text-xl sm:text-2xl'>Result:</h1>
+          <div className=' w-fu ll h-36 overflow-y-auto p-4 bg-neutral-100 rounded-lg border-neutral-300 border'>
+            {transcript}
+          </div>
+        </div>
+      )}
+      <div className='w-full flex gap-3'>
+        <button
+          onClick={handleUpload}
+          className='bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm sm:text-lg hover:bg-sky-700 transition-all duration-300 ease-in-out'
+        >
+          <AiOutlineUpload />
+          Upload
+        </button>
+        <button
+          disabled={transcript === ""}
+          onClick={() => {
+            const element = document.createElement("a");
+            const file = new Blob([transcript], { type: "text/plain" });
+            element.href = URL.createObjectURL(file);
+            element.download = "speech.txt";
+            document.body.appendChild(element); // Required for Firefox
+            element.click();
+            document.body.removeChild(element); // Clean up
+          }}
+          className={
+            transcript
+              ? "bg-sky-800 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm sm:text-lg  hover:bg-sky-700 transition-all duration-300 ease-in-out"
+              : "bg-neutral-400 text-white w-full rounded-md p-2 flex justify-center items-center gap-2 text-sm sm:text-lg"
+          }
+        >
+          <BiDownload />
+          Download
+        </button>
+      </div>
     </div>
   );
 };
